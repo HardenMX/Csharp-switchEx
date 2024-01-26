cb552a3f07c67d3bfe22f513c625a974a5d0b88b
Fmorejon <morejonff@gmail.com>
2024-01-26 09:30:51 -0600

    wrapup

--- obj/Debug/net8.0/CsEqualityOper.AssemblyInfo.cs
+++ obj/Debug/net8.0/CsEqualityOper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsEqualityOper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1824db6d185ee1441db61376e0be3c6a6f5385")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9b4886ad90fdf0dff4c285d35005a24dfcdc290")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsEqualityOper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsEqualityOper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
