2ff457686569684c93882a9ed56f489224a7729f
Fmorejon <morejonff@gmail.com>
2024-01-29 14:25:09 -0600

    start

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace Game {
+
+    class Program 
+    {
+        static void Main (string[] args) 
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
